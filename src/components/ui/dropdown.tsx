3473ed98c20c0eeffb9874e2bf75cfccd8e34c8c
Elebesunu Osereme <elebesunuosereme@gmail.com>
2026-01-19 17:04:59 +0100

    fix: fixed some deployment bugs

--- src/components/ui/dropdown.tsx
+++ src/components/ui/dropdown.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState, useEffect } from "react";
+import { useRef, useState } from "react";
 import { useClickOutside } from "../../hooks/useClickOutside";
 import ListComponent from "./listComponent";
 
